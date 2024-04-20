81d74393e327e521120d6a09fed296c9229b65b7
Andrey Svyrskyi <akkad1v@gmail.com>
2024-04-20 19:50:29 +0300

    'update'

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
-// import { nanoid } from "nanoid";
-// import { useSelector, useDispatch } from "react-redux";
 import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 import "./App.css";
-// import { addContact, deleteContact } from "./redux/contactsSlice/contactsSlice";
-// import { changeFilter } from "./redux/filtersSlice/filtersSlice";
 
 function App() {
-  // const dispatch = useDispatch();
-  // const contacts = useSelector((state) => state.contacts.items);
-  // const filter = useSelector((state) => state.filters.name);
-
-  // const onChangeFilter = (e) => {
-  //   dispatch(changeFilter(e.target.value));
-  // };
-
-  // const onAddContact = (values) => {
-  //   const finalContact = { ...values, id: nanoid() };
-  //   dispatch(addContact(finalContact));
-  // };
-
-  // const onDeleteContact = (contactId) => {
-  //   dispatch(deleteContact(contactId));
-  // };
-
-  // const filteredContacts = contacts.filter((contact) =>
-  //   contact.name.toLowerCase().includes(filter.toLowerCase())
-  // );
-
   return (
     <div>
       <h1>Phonebook</h1>
